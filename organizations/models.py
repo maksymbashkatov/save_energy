c15830f0d1ee28f6a8868e0b4615b5ee8cf0ad02
Максим Башкатов <maksymbashkatov@gmail.com>
2022-11-01 17:56:18 +0200

    Was created OrganizationObject model & related it with OrganizationProject.

--- organizations/models.py
+++ organizations/models.py
@@ -6,3 +6,12 @@ class OrganizationProject(models.Model):
         db_table = 'OrganizationProjects'
         verbose_name = 'OrganizationProject'
         verbose_name_plural = 'OrganizationProjects'
+
+
+class OrganizationObject(models.Model):
+    organization_project = models.ForeignKey(OrganizationProject, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'OrganizationObjects'
+        verbose_name = 'OrganizationObject'
+        verbose_name_plural = 'OrganizationObjects'
