233023ee3b33bef53591f80a95ae2354c7dd6c0c
Максим Башкатов <maksymbashkatov@gmail.com>
2022-11-01 19:30:47 +0200

    Was created Datapoint model & related it with OrganizationObject.

--- datapoints/models.py
+++ datapoints/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from organizations.models import OrganizationObject
 
-# Create your models here.
+
+class Datapoint(models.Model):
+    name = models.CharField(max_length=30)
+    creation_datetime = models.DateTimeField(auto_now_add=True)
+    counter_data = models.IntegerField(blank=True, null=True)
+    organization_project = models.ForeignKey(OrganizationObject, on_delete=models.CASCADE, blank=True, null=True)
+
+    class Meta:
+        db_table = 'Datapoints'
+        verbose_name = 'Datapoint'
+        verbose_name_plural = 'Datapoints'
